511c85eaba9c969952008dd3c5193d5b67c06e21
alvrs <alvarius@lattice.xyz>
2022-04-18 16:53:25 +0200

    Step 6.3: Add particles on attack

--- client/src/systems/LifeSystem.ts
+++ client/src/systems/LifeSystem.ts
@@ -1,12 +1,12 @@
 import { defineReactionSystem, defineUpdateQuery, getComponentValue, Has } from "@latticexyz/mobx-ecs";
-import { drawBar, worldCoordToPixelTopLeft } from "@latticexyz/phaser-middleware";
+import { drawBar, explode, worldCoordToPixelTopLeft } from "@latticexyz/phaser-middleware";
 import { Context } from "../types";
 
 export function createLifeSystem(context: Context) {
   const {
     world,
     components: { Life, Position },
-    phaser: { map, graphicsPool },
+    phaser: { map, graphicsPool, particles },
   } = context;
 
   const lifePositionQuery = defineUpdateQuery(world, [Has(Life), Has(Position)]);
@@ -30,6 +30,23 @@ export function createLifeSystem(context: Context) {
         lifebar.setDepth(5);
         drawBar(lifebar, life.value1, life.value2, map.tileWidth, 0xeb4926);
         lifebar.setPosition(pixelCoord.x, pixelCoord.y + 2);
+      }
+    }
+  );
+
+  const lifeQuery = defineUpdateQuery(world, [Has(Life)]);
+  defineReactionSystem(
+    world,
+    () => lifeQuery.get(),
+    async (entities) => {
+      for (const entity of entities) {
+        const life = getComponentValue(Life, entity);
+        const position = getComponentValue(Position, entity);
+
+        if (!life || !position) return;
+
+        // Blood explosion
+        explode(position, particles, map, 0x700000);
       }
     }
   );
