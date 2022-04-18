25503bda510a0565c75b65ee7f468601847cb0b8
alvrs <alvarius@lattice.xyz>
2022-04-18 17:12:13 +0200

    Step 6.6: Add React UI for lost entities and lost game

--- client/src/react/App.tsx
+++ client/src/react/App.tsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Context } from "../types";
 import styled from "styled-components";
 import { observer } from "mobx-react";
+import { reaction } from "mobx";
+import { defineExitQuery, defineQuery, exists, Has, HasValue } from "@latticexyz/mobx-ecs";
+import { Modal } from "./Modal";
 
-export const App: React.FC<{ context: Context }> = observer(() => {
+export const App: React.FC<{ context: Context }> = observer(({ context }) => {
+  const [modalText, setModalText] = useState("");
+
+  useEffect(() => {
+    const {
+      world,
+      components: { OwnedBy, Heart },
+    } = context;
+    const lostEntityQuery = defineExitQuery(world, [HasValue(OwnedBy, { value: context.signer.address })]);
+    const ownedByQuery = defineQuery([HasValue(OwnedBy, { value: context.signer.address })]);
+    return reaction(
+      () => lostEntityQuery.get(),
+      (lostEntities) => {
+        if (lostEntities.size == 0) return;
+        // If the player doesn't own a heart anymore, the game is lost
+        if (
+          ownedByQuery.get().size > 0 &&
+          !exists([Has(Heart), HasValue(OwnedBy, { value: context.signer.address })])
+        ) {
+          setModalText("Game over");
+        } else {
+          // Flash a quick modal if the player lost an entity
+          setModalText("Entity lost");
+          setTimeout(() => setModalText(""), 300);
+        }
+      }
+    );
+  }, []);
+
   return (
     <Container>
-      <Inner></Inner>
+      <Inner>{modalText && <Modal text={modalText} fontSize={"80px"} bgColor={"rgb(255,0,0,0.2)"} />}</Inner>
     </Container>
   );
 });
